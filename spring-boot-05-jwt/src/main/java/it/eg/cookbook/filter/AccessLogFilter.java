a0622f5c259c74b5e75048039c9d085050ae69cc
Enrico Grillini <enrico.grillini@gmail.com>
2023-04-21 19:30:43 +0200

    Access log

--- spring-boot-05-jwt/src/main/java/it/eg/cookbook/filter/AccessLogFilter.java
+++ spring-boot-05-jwt/src/main/java/it/eg/cookbook/filter/AccessLogFilter.java
@@ -1,14 +1,20 @@
 package it.eg.cookbook.filter;
 
-import jakarta.servlet.*;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.WebUtils;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.UUID;
@@ -16,36 +22,55 @@ import java.util.UUID;
 @Slf4j
 @Component
 @Order(1)
-public class AccessLogFilter implements Filter {
+public class AccessLogFilter extends OncePerRequestFilter {
 
     public static final String CORRELATION_ID_NAME = "X-Request-ID";
     public static final String ACTUATOR = "/actuator";
 
+    public static final int CONTENT_CACHE_LIMIT = 1000;
+
+    protected boolean isAsyncDispatch(HttpServletRequest request) {
+        return DispatcherType.ASYNC.equals(request.getDispatcherType());
+    }
+
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse resp = (HttpServletResponse) response;
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        HttpServletRequest requestToUse = request;
+        boolean isFirstRequest = !this.isAsyncDispatch(request);
+        if (isFirstRequest && !(request instanceof ContentCachingRequestWrapper)) {
+            requestToUse = new ContentCachingRequestWrapper(request, CONTENT_CACHE_LIMIT);
+        }
+
         Instant start = Instant.now();
 
-        String correlationId = req.getHeader(CORRELATION_ID_NAME);
+        String correlationId = requestToUse.getHeader(CORRELATION_ID_NAME);
         if (correlationId == null || "".equals(correlationId.trim())) {
             correlationId = generateUniqueCorrelationId();
         }
 
-        resp.setHeader(CORRELATION_ID_NAME, correlationId);
+        response.setHeader(CORRELATION_ID_NAME, correlationId);
         try (MDC.MDCCloseable m = MDC.putCloseable(CORRELATION_ID_NAME, correlationId)) {
-            boolean doLog = req.getRequestURI() == null || !req.getRequestURI().startsWith(ACTUATOR);
+            boolean doLog = requestToUse.getRequestURI() == null || !requestToUse.getRequestURI().startsWith(ACTUATOR);
 
             // Access Log IN
             if (doLog) {
-                log.info("IN  - method: {}, URI: {}, protocol {}, host: {}", req.getMethod(), req.getRequestURI(), req.getProtocol(), req.getRemoteHost());
+                log.info("IN  | {} {}", requestToUse.getMethod(), requestToUse.getRequestURI());
             }
 
-            chain.doFilter(request, response);
+            filterChain.doFilter(requestToUse, response);
 
             // Access Log OUT
             if (doLog) {
-                log.info("OUT - method: {}, URI: {}, protocol {}, host: {}, status {}, duration {}", req.getMethod(), req.getRequestURI(), req.getProtocol(), req.getRemoteHost(), resp.getStatus(), ChronoUnit.MILLIS.between(start, Instant.now()));
+                log.info("OUT | {} {} | status {}, duration {}", requestToUse.getMethod(), requestToUse.getRequestURI(), response.getStatus(), ChronoUnit.MILLIS.between(start, Instant.now()));
+                String queryString = requestToUse.getQueryString();
+                if (queryString != null) {
+                    log.info("    | req query:   {}", queryString);
+                }
+
+                String payload = getMessagePayload(requestToUse);
+                if (payload != null) {
+                    log.debug("    | req payload: {}", payload);
+                }
             }
         }
     }
@@ -54,6 +79,24 @@ public class AccessLogFilter implements Filter {
         return new StringBuilder().append("generated:")
                 .append(UUID.randomUUID())
                 .toString();
+    }
+
+    private String getMessagePayload(HttpServletRequest request) {
+        ContentCachingRequestWrapper wrapper = (ContentCachingRequestWrapper) WebUtils.getNativeRequest(request, ContentCachingRequestWrapper.class);
+        if (wrapper != null) {
+            byte[] buf = wrapper.getContentAsByteArray();
+            if (buf.length > 0) {
+                int length = Math.min(buf.length, CONTENT_CACHE_LIMIT);
+
+                try {
+                    return new String(buf, 0, length, wrapper.getCharacterEncoding()).replace("\n", "").replace("\r", "");
+                } catch (UnsupportedEncodingException var6) {
+                    return "[unknown]";
+                }
+            }
+        }
+
+        return null;
     }
 
 }
