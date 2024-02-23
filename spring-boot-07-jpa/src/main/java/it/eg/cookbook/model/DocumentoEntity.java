13947d4d735fdacb96049f4e463d91beb0913a79
Enrico Grillini <enrico.grillini@gmail.com>
2024-02-23 14:37:38 +0100

    refactor

---
+++ spring-boot-07-jpa/src/main/java/it/eg/cookbook/model/DocumentoEntity.java
@@ -0,0 +1,24 @@
+package it.eg.cookbook.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "documento")
+public class DocumentoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_id_documento")
+    @SequenceGenerator(name = "seq_id_documento", allocationSize = 1)
+    private Long id;
+
+    private String name;
+    private String description;
+    private LocalDate data;
+    private String updateBy;
+}
