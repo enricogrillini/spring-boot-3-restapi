47c66fdcfea4f8838e9dce503ec198817e44c31c
Enrico Grillini <enrico.grillini@gmail.com>
2024-03-27 12:46:44 +0100

    import

--- spring-boot-07-jpa/src/main/java/it/eg/cookbook/filter/AccessLogFilter.java
+++ spring-boot-07-jpa/src/main/java/it/eg/cookbook/filter/AccessLogFilter.java
@@ -4,7 +4,6 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.MDC;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
